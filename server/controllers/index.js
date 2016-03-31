db2b43b1cf9288e45dd70481114f01c08f0f480f
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-03-31 16:55:17 -0700

    added closing connection

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,23 +10,27 @@ router.get('/', function(req, res) {
 
 router.post('/email', function(req, res){
   var db2;
+  var connString;
 
   if (process.env.VCAP_SERVICES) {
       var env = JSON.parse(process.env.VCAP_SERVICES);
       db2 = env['sqldb'][1].credentials;
+      connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
+
   }
   else {
-
+    connString = process.env.DATABASE_URL;
   }
 
-  var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
   ibmdb.open(connString, function(error, conn){
     if (error){
-			 res.send("error occurred " + err.message);
+			 res.send("error occurred " + error.message);
 			}
       else{
           conn.query('INSERT INTO MYTABLE (ADDRESS,COMPANYDESCRIPTION,COMAPANYLOGO,COMPANYNAME,COMPANYNUMBER,COMPANYPOINTS,COMPANYSTATUS,COMPANYSTATUS,COMPANYURL,EMAIL,NAME) VALUES(\'2016 BLAKE ST\',\'YOYO\',\'YO\',\'H\',\'123'+',1,'+'\'HELL\',\'YO\',\'HALAH@GMAIL.COM\',\'HALA\')\;', function(err, tables, moreResultSets){
-
+            conn.close(function(){
+					         res.send('success yo!');
+					      });
             });
       }
   });
